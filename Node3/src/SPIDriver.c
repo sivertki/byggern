1f9145f34fcd3ab4085f6afd84a661198b849aea
Sivert Kittelsen <sivertkittelsen@gmail.com>
2019-11-10 12:56:40 +0100

    Maybe this will help

--- Node3/src/SPIDriver.c
+++ Node3/src/SPIDriver.c
@@ -11,11 +11,11 @@ void SPI_transmit(char cData) {
 
 void SPI_init() {
   /* Set MOSI and SCK and SS as output, all others input */
-  DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK) | (1<<DD_SS);
+  DDR_SPI |= (1<<DD_MOSI)|(1<<DD_SCK) | (1<<DD_SS);
   /* set MISO as input */
   DDR_SPI &= ~(1<<DD_MISO);
   /* Enable SPI, Master, set clock rate fck/16 */
-  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+  SPCR = (1<<SPE)|(1<<MSTR);//|(1<<SPR0);
 }
 
 char SPI_receive(void){
