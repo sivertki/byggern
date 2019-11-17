0cb14ae886341d5365994bd922407221c3458f34
Sivert Kittelsen <sivertkittelsen@gmail.com>
2019-11-17 16:04:20 +0100

    remove timer usage from Node3

--- Node3/src/IODriver.c
+++ Node3/src/IODriver.c
@@ -7,29 +7,12 @@ void IO_init() {
     //Set data direction of buzzer pin
     DDRC |= (1<<BUZZER_PIN);
 
-    //Set up timer settings
-    buzzer_timer_init();
-
 }
 
-void buzzer_timer_init() {
-    //set mode to CTC
-    TCCR2A |= (1<<WGM21);
-
-    //With a 4MHz oscillator, fused prescaler of 8, each output compare match
-    //with 1024 prescaler at 250 is equavalent to 512ms. 
-
-    OCR2A = 0b11111010;
-    
-    //enable interrupt on ouput compare match A
-    TIMSK2 |= (1<<OCIE2A);
+void IO_sound_buzzer() {
+    PORTC |= (1<<PC3);
 }
 
-void IO_buzzer_timer_enable() {
-    //set prescaler to 1024. Enables timer
-    TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);
-}
-
-void IO_buzzer_timer_disable() {
-    TCCR2B &= ~((1<<CS22) | (1<<CS21) | (1<<CS20));
+void IO_silence_buzzer() {
+    PORTC &= ~(1<<PC3);
 }
--- Node3/src/IODriver.h
+++ Node3/src/IODriver.h
@@ -2,5 +2,5 @@
 #define BUZZER_PIN PC3
 
 void IO_init();
-void IO_buzzer_timer_enable();
-void IO_buzzer_timer_disable()
+void IO_sound_buzzer();
+void IO_silence_buzzer();
